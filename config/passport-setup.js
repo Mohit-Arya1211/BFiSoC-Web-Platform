5c1bb44c0c35604fbbd6455d1a5c4eac9f6eefd5
Mohit Arya <43243827+Mohit-Arya1211@users.noreply.github.com>
2019-04-12 20:21:47 +0530

    Updated Github login

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,17 +2,14 @@ const passport=require("passport");
 const GithubStrategy=require("passport-github");
 const keys=require("./keys");
 const User=require("../models/user-model");
-
 passport.serializeUser((user,done)=>{
   done(null,user.id);
 });
-
 passport.deserializeUser((id,done)=>{
   User.findById(id).then((user)=>{
     done(null,user);
   })
 });
-
 passport.use(new GithubStrategy({
     //optoion for google strategy
     callbackURL:"/auth/github/redirect",
